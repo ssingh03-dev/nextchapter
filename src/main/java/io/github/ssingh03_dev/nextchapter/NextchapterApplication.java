016c67cd28766aed3fb04f526eea8a982a07cacc
Sahajdeep Singh <impulseVD@hotmail.com>
2026-05-28 03:19:03 -0400

    Initial commit

---
+++ src/main/java/io/github/ssingh03_dev/nextchapter/NextchapterApplication.java
@@ -0,0 +1,13 @@
+package io.github.ssingh03_dev.nextchapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextchapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextchapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ssingh03_dev/nextchapter/NextchapterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ssingh03_dev.nextchapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextchapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
